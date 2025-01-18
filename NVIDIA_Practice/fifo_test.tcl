ff84d809f3c15473f421cdfcecaf5838759bcb55
Abiola Bolaji <ab2113@princeton.edu>
2025-01-18 14:10:44 -0500

    d

--- NVIDIA_Practice/fifo_test.tcl
+++ NVIDIA_Practice/fifo_test.tcl
@@ -14,7 +14,7 @@ assert {!rst_n |-> ##1 (data_out == 0)}
 # when full if write is enabled  count value shouldnt change
 # when empty if read is enabled count value shouldnt change
 assert {full && w_en && !r_en |-> ##1 $stable(count)}
-assert{empty && r_en && !w_en |-> ##1 $stable(count)}
+assert {empty && r_en && !w_en |-> ##1 $stable(count)}
 
 # Set the time limit to 1 hour (3600 seconds)
 set_prove_time_limit 3600
