90e2265b1d4793f284f97f76de5ce3fa0b255443
Abiola Bolaji <ab2113@princeton.edu>
2025-01-19 16:45:54 -0500

    d

--- NVIDIA_Practice/Vending_Machine.tcl
+++ NVIDIA_Practice/Vending_Machine.tcl
@@ -15,7 +15,7 @@ assert {reset & coin_in == 2'b01 |-> ##1 Total_Amount == 10}
 assert {reset & coin_in == 2'b10 |-> ##1 Total_Amount == 25}
 assert {reset & coin_in == 2'b11 |-> ##1 Total_Amount == 100}
 
-assume {$past(coin_in) == 2'b10}
+# assume {$past(coin_in) == 2'b10}
 assert {coin_in == 2'b11 |-> ##1 Total_Amount == 125}
 
 # Set the time limit to 1 hour (3600 seconds)
