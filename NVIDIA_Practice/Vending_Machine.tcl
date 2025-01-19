601d3d0eb19f54a5ad475fb53ca007bce2be6244
Abiola Bolaji <ab2113@princeton.edu>
2025-01-19 16:41:44 -0500

    d

--- NVIDIA_Practice/Vending_Machine.tcl
+++ NVIDIA_Practice/Vending_Machine.tcl
@@ -7,10 +7,10 @@ reset reset
 
 
 # check reset properties 
-assert {reset |-> ##1 (Total_Amount == 0) & (dispense == 0)}
+assert {reset |-> ##1 ((Total_Amount == 0) & (dispense == 0))}
 
 # check total amount properties
-assert {coin_in == 2'b00 |-> ##1 Total_Amount == 5}
+assert {coin_in == 2'b00 |-> ##1 (Total_Amount == 5)}
 assert {coin_in == 2'b01 |-> ##1 Total_Amount == 10}
 assert {coin_in == 2'b10 |-> ##1 Total_Amount == 25}
 assert {coin_in == 2'b11 |-> ##1 Total_Amount == 100}
