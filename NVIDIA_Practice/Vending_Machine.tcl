a3e91680abf1c97d7ab82363e3f9cd5a67821e09
Abiola Bolaji <ab2113@princeton.edu>
2025-01-19 16:38:03 -0500

    d

--- NVIDIA_Practice/Vending_Machine.tcl
+++ NVIDIA_Practice/Vending_Machine.tcl
@@ -15,5 +15,5 @@ assert {coin_in == 2'b01 |-> ##1 Total_Amount == 10}
 assert {coin_in == 2'b10 |-> ##1 Total_Amount == 25}
 assert {coin_in == 2'b11 |-> ##1 Total_Amount == 100}
 
-assume {$past(coin_in) == 2'b10}
+# assume {$past(coin_in) == 2'b10}
 assert {coin_in == 2'b11 |-> ##1 Total_Amount == 125}
